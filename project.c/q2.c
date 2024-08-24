437f7a732b67d100421cc79e75ba374065206f47
Sapkalpratiksha <sapkalpratiksha29@gmail.com>
2024-08-24 11:57:19 +0530

    add change

--- project.c/q2.c
+++ project.c/q2.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
-int mai()
+int main()
 
 {
+    int baseSalary,HRA,DA,TA,grossSalary;
+    printf("enter the base salary:-");
+    scanf("%d",&baseSalary);
+
+    HRA=baseSalary*10/100;
+    DA=baseSalary*5/100;
+    TA=baseSalary*8/100;
+
+    grossSalary=baseSalary+HRA+DA+TA;
+
+printf("gross salary is %d",grossSalary);
     
     return 0;
 }
---
+++ project.c/q3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+ int firstAngle,secondAngle,thirdAngle;
+    
+    return 0;
+}
